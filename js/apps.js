b5700d9d5a1ecb9c39269adc3334a6cf0f4038b6
Keyla N <k.nunez2017@gmail.com>
2023-06-08 19:04:18 -0400

    Finished

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+const menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+let mainEl = document.querySelector('main')
+mainEl.style.backgroundColor = 'var(--main-bg)'
+mainEl.classList.add('flex-ctr')
+console.log(mainEl);
+
+let topMenuEl = document.getElementById('top-menu')
+topMenuEl.style.height = '100%'
+topMenuEl.style.background = 'var(--top-menu-bg)'
+topMenuEl.classList.add('flex-around')
+console.log(topMenuEl);
+
+menuLinks.forEach(function(link){
+    let a = document.createElement('a');
+    a.setAttribute('href', link.href);
+    a.innerText = link.text;
+    topMenuEl.appendChild(a);
+});
