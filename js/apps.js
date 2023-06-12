ed51606bbbde73083cb00d4c4d1b69b78a0cb674
Keyla N <k.nunez2017@gmail.com>
2023-06-11 23:58:14 -0400

    finished hw 5

--- js/apps.js
+++ js/apps.js
@@ -27,21 +27,83 @@ topMenuEl.style.background = 'var(--top-menu-bg)'
 topMenuEl.classList.add('flex-around')
 console.log(topMenuEl);
 
-let topMenuLinks = menuLinks.forEach(function(link){
-    let a = document.createElement('a');
-    a.setAttribute('href', link.href);
-    a.innerText = link.text;
-    topMenuEl.appendChild(a);
-});
-
-topMenuEl.addEventListener('click', function()){
-
-}
-let showingSubMenu = false
-
 let subMenuEl = document.getElementById('sub-menu')
 subMenuEl.style.height = '100%'
 subMenuEl.style.background = 'var(--sub-menu-bg)'
 subMenuEl.classList.add('flex-around')
 subMenuEl.style.position = 'absolute'
 subMenuEl.style.top = '0'
+
+menuLinks.forEach(function(link){
+    let a = document.createElement('a');
+    a.setAttribute('href', link.href);
+    a.innerText = link.text;
+    topMenuEl.appendChild(a);
+    console.log(a);
+});
+
+let topMenuLinks = topMenuEl.querySelectorAll('a');
+console.log(topMenuLinks);
+
+let showingSubMenu = false;
+
+topMenuEl.addEventListener('click', topLinks)
+function topLinks(event){
+  event.preventDefault();
+  if (event.target.tagName !== 'A')
+    
+
+    if (event.target.classList.contains('active')){
+      event.target.classList.remove('active');
+      showingSubMenu = false;
+      subMenuEl.style.top = 0;
+      return;
+    };
+
+    for(let i = 0; i < topMenuLinks.length; i++){
+      topMenuLinks[i].classList.remove('active');
+    }
+    
+    event.target.classList.add('active');
+    
+    let linkData = menuLinks.find(function(linkObject){
+      return linkObject.text === event.target.textContent
+    })
+    console.log(linkData);
+
+    showingSubMenu = 'subLinks' in linkData;
+    console.log(showingSubMenu);
+
+    if(showingSubMenu === true){
+      buildSubMenu(linkData.subLinks);
+      subMenuEl.style.top = '100';
+    }else{subMenuEl.style.top = '0';
+    mainEl.innerHTML = '<h1>About</h1>'}
+}
+
+function buildSubMenu(subLinks){
+  subLinks.forEach(function(subMenuObj){
+    subMenuEl.innerHTML = "";
+    let subMenuObjEl = document.createElement('a');
+  subMenuObjEl.setAttribute('href',subMenuObj.href);
+  subMenuObjEl.innerText = subMenuObj.text;
+  subMenuEl.appendChild(subMenuObjEl);
+  subMenuEl.style.top = '100%';
+  })
+  return;
+}
+
+subMenuEl.addEventListener('click', subMenuClick);
+function subMenuClick(event){
+  event.preventDefault();
+  if(event.target.tagName !== 'A')
+  console.log(event.target);
+
+  showingSubMenu = false;
+  subMenuEl.style.top = '0';
+  mainEl.innterHTML = '<h1>${event.target.innerText}</h1>';
+}
+
+topMenuLinks.forEach(function(subMenuObj){
+  subMenuObj.classList.remove('active');
+})
